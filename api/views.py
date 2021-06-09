fb4adb7f286ecf4fc797fd8e3115ca8cd25c5921
anthony <anthonybonfils3@gmail.com>
2021-06-09 14:22:42 +0200

    maj app, debbug video, bottom

--- api/views.py
+++ api/views.py
@@ -12,35 +12,36 @@ import pickle
 
 app = Flask(__name__)
 
+filename = "api/pkl_objets/model.pickle"
+model = pickle.load(open(filename, 'rb'))
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/result', methods = ['POST'])
 def result():
-    filename = "models/model.pickle"
-    model = pickle.load(open(filename, 'rb'))
 
     name = request.form.get("name")
-    age = request.form.get("age")*365.25
-    gender = request.form.get("gender")
-    height = request.form.get("height")
-    weight = request.form.get("weight")
-    ap_hi = request.form.get("ap_hi")
-    ap_lo = request.form.get("ap_lo")
-    cholesterol= request.form.get("cholesterol")
-    glucose = request.form.get("glucose")
-    smoke = request.form.get("smoke")
-    alcohol = request.form.get("alcohol")
-    activity = request.form.get("activity")
+    age = int(request.form.get("age"))
+    gender = int(request.form.get("gender"))
+    height = int(request.form.get("height"))
+    weight = int(request.form.get("weight"))
+    ap_hi = int(request.form.get("ap_hi"))
+    ap_lo = int(request.form.get("ap_lo"))
+    cholesterol= int(request.form.get("cholesterol"))
+    glucose = int(request.form.get("glucose"))
+    smoke = int(request.form.get("smoke"))
+    alcohol = int(request.form.get("alcohol"))
+    activity = int(request.form.get("activity"))
 
-    person = [[age, gender, height, weight, ap_hi, ap_lo, 
+    person = [[age*365.25, gender, height, weight, ap_hi, ap_lo, 
             cholesterol, glucose, smoke, alcohol, activity]]
 
     pred = model.predict(person)
     proba = model.predict_proba(person)
 
-    return render_template('result.html', name = name, pred=pred[0, 1].round(3), proba=proba)
+    return render_template('result.html', name = name, pred=pred, proba=round(proba[0, 1],3))
 
 if __name__ == "__main__":
     app.run()
